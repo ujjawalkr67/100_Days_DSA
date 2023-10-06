995edbb252c4aff7e740979c6723c36b983f0686
Ujjawal Kumar <ujjawalkr67@gmail.com>
2023-10-06 22:42:15 +0530

    day 1

---
+++ Day_1/01pattern.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main (){
+int rows;
+cout<<"Enter the no. of rows"<<endl;
+cin>>rows;
+for(int i=0;i<rows;i++){
+    for (int j=0;j<rows;j++){
+        cout<<"* ";
+    }
+    cout<<endl;
+}
+}
